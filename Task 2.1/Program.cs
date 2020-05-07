6f776a04b5561c55686039a888d11873a9277fe4
Карина Дроздова <K.Drozd33@yandex.ru>
2020-05-07 21:39:58 +0300

    Добавьте файлы проекта.

---
+++ Task 2.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите х: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            if (y <= x && y >= 0 && x <= 23)
+            {
+                if (y == x || y == 0 || x == 23)
+                {
+                    Console.WriteLine("На границе");
+                }
+                else Console.WriteLine("Да");
+            }
+            else Console.WriteLine("Нет");
+
+            Console.ReadKey();
+        }
+    }
+}
